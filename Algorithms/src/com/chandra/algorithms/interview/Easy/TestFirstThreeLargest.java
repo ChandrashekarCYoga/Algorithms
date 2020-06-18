c2f20e17fcaef07422e15276a33ba8428ff27518
ChandrashekarCYoga <chandrashekar.c@gmail.com>
2020-06-18 23:14:32 +0200

    Find first three largest numbers

---
+++ Algorithms/src/com/chandra/algorithms/interview/Easy/TestFirstThreeLargest.java
@@ -0,0 +1,29 @@
+package com.chandra.algorithms.interview.Easy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestFirstThreeLargest {
+
+	@Test
+	void test() {
+		int[] expected = { 18, 141, 541 };
+		assertTrue(compare(FindThreeLargestNumbers
+				.findThreeLargestNumbers(new int[] { 141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7 }), expected));
+
+	}
+
+	public boolean compare(int[] arr1, int[] arr2) {
+		if (arr1.length != arr2.length) {
+			return false;
+		}
+		for (int i = 0; i < arr1.length; i++) {
+			if (arr1[i] != arr2[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
